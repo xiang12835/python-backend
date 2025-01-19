9ed6d755a336b4472e1fe2500f8899c3438eaf6e
fishane <953762287@qq.com>
2025-01-19 20:22:02 +0800

    add_systemuseradmin

--- app/user/admin.py
+++ app/user/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from app.user.models.user_model import SystemUser
 
 # Register your models here.
+
+
+class SystemUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'date_joined')
+    search_fields = ('username', 'email')
+    list_filter = ('date_joined',)
+
+
+admin.site.register(SystemUser, SystemUserAdmin)
+
